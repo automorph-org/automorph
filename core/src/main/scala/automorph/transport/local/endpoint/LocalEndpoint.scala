62341edc3918c4775b8e2be3dcb0e9814f7a0c97
Martin Ockajak <m@polytree.org>
2023-08-28 18:23:28 +0200

    Add LocalEndpoint methods - context Remove LocalEndpoint fields - defaultContext

--- core/src/main/scala/automorph/transport/local/endpoint/LocalEndpoint.scala
+++ core/src/main/scala/automorph/transport/local/endpoint/LocalEndpoint.scala
@@ -11,8 +11,6 @@ import automorph.spi.{EffectSystem, EndpointTransport, RequestHandler}
  *   Creates a local endpoint transport plugin
  * @param effectSystem
  *   effect system plugin
- * @param defaultContext
- *   default request context
  * @param handler
  *   RPC request handler
  * @tparam Effect
@@ -22,7 +20,6 @@ import automorph.spi.{EffectSystem, EndpointTransport, RequestHandler}
  */
 final case class LocalEndpoint[Effect[_], Context](
   effectSystem: EffectSystem[Effect],
-  defaultContext: Context,
   handler: RequestHandler[Effect, Context] = RequestHandler.dummy[Effect, Context],
 ) extends EndpointTransport[Effect, Context, Unit] {
 
@@ -31,4 +28,41 @@ final case class LocalEndpoint[Effect[_], Context](
 
   override def withHandler(handler: RequestHandler[Effect, Context]): LocalEndpoint[Effect, Context] =
     copy(handler = handler)
+}
+
+object LocalEndpoint {
+
+  /**
+   * Local endpoint transport builder.
+   *
+   * @constructor
+   *   Creates a new local endpoint transport builder.
+   * @tparam Context
+   *   RPC message context type
+   */
+  final case class LocalEndpointBuilder[Context]() {
+    /**
+     * Creates a new local endpoint transport plugin with specified effect system.
+     *
+     * @param effectSystem
+     *   effecty protocol plugin
+     * @tparam Effect
+     *   effect type
+     * @return
+     *   RPC endpoint builder
+     */
+    def effectSystem[Effect[_]](effectSystem: EffectSystem[Effect]): LocalEndpoint[Effect, Context] =
+      LocalEndpoint(effectSystem)
+  }
+
+  /**
+   * Creates a new local endpoint transport builder with specified RPC message contex type.
+   *
+   * @tparam Context
+   *   RPC message context type
+   * @return
+   *   local endpoint transport builder
+   */
+  def context[Context]: LocalEndpointBuilder[Context] =
+    LocalEndpointBuilder()
 }
