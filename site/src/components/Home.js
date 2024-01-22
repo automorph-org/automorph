53f9c4fa1fae888bab5b2a30e282590fedd85bcc
Martin Ockajak <m@polytree.org>
2024-01-22 18:04:44 +0100

    Update documentation

--- site/src/components/Home.js
+++ site/src/components/Home.js
@@ -66,7 +66,7 @@ const features = [
     description: (
         <>
           Utilize discovery functions providing <a href='https://spec.open-rpc.org'>OpenRPC</a> 1.3+ and <a
-            href='https://www.openapis.org'>OpenAPI</a> 3.1+ schema for exposed APIs.
+            href='https://www.openapis.org'>OpenAPI</a> 3.1+ schemas for exposed APIs.
         </>
     ),
   },
