bc6140a8fa4b88f93e649894412befbd61e8648c
Martin Ockajak <m@polytree.org>
2024-01-24 19:02:28 +0100

    .

--- test/system/src/main/scala/test/core/CoreTest.scala
+++ test/system/src/main/scala/test/core/CoreTest.scala
@@ -89,126 +89,126 @@ trait CoreTest extends BaseTest {
               }
               "Complex API" - {
                 val apis = (fixture.complexApi, complexApi)
-//                "method0" in {
-//                  check { (_: Unit) =>
-//                    consistent(apis)(_.method0())
-//                  }
-//                }
-//                "method1" in {
-//                  check { (_: Unit) =>
-//                    consistent(apis)(_.method1())
-//                  }
-//                }
-//                "method2" in {
-//                  check { (a0: String) =>
-//                    consistent(apis)(_.method2(a0))
-//                  }
-//                }
-//                "method3" in {
-//                  check { (a0: Float, a1: Long, a2: Option[List[Int]]) =>
-//                    consistent(apis)(_.method3(a0, a1, a2))
-//                  }
-//                }
-//                "method4" in {
-//                  check { (a0: Double, a1: Byte, a2: Map[String, Int], a3: Option[String]) =>
-//                    consistent(apis)(_.method4(BigDecimal(a0), a1, a2, a3))
-//                  }
-//                }
-//                "method5" in {
-//                  check { (a0: Boolean, a1: Short, a2: List[Int]) =>
-//                    consistent(apis)(_.method5(a0, a1)(a2))
-//                  }
-//                }
-//                "method6" in {
-//                  check { (a0: Record, a1: Double) =>
-//                    consistent(apis)(_.method6(a0, a1))
-//                  }
-//                }
-//                "method7" in {
-//                  check { (a0: Record, a1: Boolean, context: Context) =>
-//                    implicit val usingContext: Context = context
-//                    consistent(apis)(_.method7(a0, a1))
-//                  }
-//                }
-//                "method8" in {
-//                  check { (a0: Record, a1: String, a2: Option[Double]) =>
-//                    consistent(apis) { api =>
-//                      system.map(api.method8(a0, a1, a2)) { result =>
-//                        s"${result.result} - ${result.context.getClass.getName}"
-//                      }
-//                    }
-//                  }
-//                }
-//                "method9" in {
-//                  check { (a0: String) =>
-//                    val (testedApi, referenceApi) = apis
-//                    val result = Try(run(testedApi.method9(a0))).toEither
-//                    val expected = Try(run(referenceApi.method9(a0))).toEither
-//                    val expectedErrorMessage = expected.swap.map(error =>
-//                      s"[${error.getClass.getSimpleName}] ${Option(error.getMessage).getOrElse("")}"
-//                    )
-//                    expectedErrorMessage == result.swap.map(_.getMessage)
-//                  }
-//                }
+                "method0" in {
+                  check { (_: Unit) =>
+                    consistent(apis)(_.method0())
+                  }
+                }
+                "method1" in {
+                  check { (_: Unit) =>
+                    consistent(apis)(_.method1())
+                  }
+                }
+                "method2" in {
+                  check { (a0: String) =>
+                    consistent(apis)(_.method2(a0))
+                  }
+                }
+                "method3" in {
+                  check { (a0: Float, a1: Long, a2: Option[List[Int]]) =>
+                    consistent(apis)(_.method3(a0, a1, a2))
+                  }
+                }
+                "method4" in {
+                  check { (a0: Double, a1: Byte, a2: Map[String, Int], a3: Option[String]) =>
+                    consistent(apis)(_.method4(BigDecimal(a0), a1, a2, a3))
+                  }
+                }
+                "method5" in {
+                  check { (a0: Boolean, a1: Short, a2: List[Int]) =>
+                    consistent(apis)(_.method5(a0, a1)(a2))
+                  }
+                }
+                "method6" in {
+                  check { (a0: Record, a1: Double) =>
+                    consistent(apis)(_.method6(a0, a1))
+                  }
+                }
+                "method7" in {
+                  check { (a0: Record, a1: Boolean, context: Context) =>
+                    implicit val usingContext: Context = context
+                    consistent(apis)(_.method7(a0, a1))
+                  }
+                }
+                "method8" in {
+                  check { (a0: Record, a1: String, a2: Option[Double]) =>
+                    consistent(apis) { api =>
+                      system.map(api.method8(a0, a1, a2)) { result =>
+                        s"${result.result} - ${result.context.getClass.getName}"
+                      }
+                    }
+                  }
+                }
+                "method9" in {
+                  check { (a0: String) =>
+                    val (testedApi, referenceApi) = apis
+                    val result = Try(run(testedApi.method9(a0))).toEither
+                    val expected = Try(run(referenceApi.method9(a0))).toEither
+                    val expectedErrorMessage = expected.swap.map(error =>
+                      s"[${error.getClass.getSimpleName}] ${Option(error.getMessage).getOrElse("")}"
+                    )
+                    expectedErrorMessage == result.swap.map(_.getMessage)
+                  }
+                }
               }
               "Invalid API" - {
                 val api = fixture.invalidApi
-//                "Function not found" in {
-//                  val error = intercept[FunctionNotFound](run(api.nomethod(""))).getMessage.toLowerCase
-//                  error.should(include("function not found"))
-//                  error.should(include("nomethod"))
-//                }
-//                "Optional arguments" in {
-//                  run(api.method3(0, Some(0)))
-//                }
-//                "Malformed argument" in {
-//                  val error = intercept[InvalidArguments] {
-//                    run(api.method4(BigDecimal(0), Some(true), None))
-//                  }.getMessage.toLowerCase
-//                  error.should(include("malformed argument"))
-//                  error.should(include("p1"))
-//                }
-//                "Missing arguments" in {
-//                  val error = intercept[InvalidArguments] {
-//                    run(api.method5(p0 = true, 0))
-//                  }.getMessage.toLowerCase
-//                  error.should(include("missing argument"))
-//                  error.should(include("p2"))
-//                }
-//                "Redundant arguments" in {
-//                  val error = intercept[InvalidArguments](run(api.method1(""))).getMessage.toLowerCase
-//                  error.should(include("redundant arguments"))
-//                  error.should(include("0"))
-//                }
-//                "Malformed result" in {
-//                  val error = intercept[InvalidResponse] {
-//                    run(api.method2(""))
-//                  }.getMessage.toLowerCase
-//                  error.should(include("malformed result"))
-//                }
+                "Function not found" in {
+                  val error = intercept[FunctionNotFound](run(api.nomethod(""))).getMessage.toLowerCase
+                  error.should(include("function not found"))
+                  error.should(include("nomethod"))
+                }
+                "Optional arguments" in {
+                  run(api.method3(0, Some(0)))
+                }
+                "Malformed argument" in {
+                  val error = intercept[InvalidArguments] {
+                    run(api.method4(BigDecimal(0), Some(true), None))
+                  }.getMessage.toLowerCase
+                  error.should(include("malformed argument"))
+                  error.should(include("p1"))
+                }
+                "Missing arguments" in {
+                  val error = intercept[InvalidArguments] {
+                    run(api.method5(p0 = true, 0))
+                  }.getMessage.toLowerCase
+                  error.should(include("missing argument"))
+                  error.should(include("p2"))
+                }
+                "Redundant arguments" in {
+                  val error = intercept[InvalidArguments](run(api.method1(""))).getMessage.toLowerCase
+                  error.should(include("redundant arguments"))
+                  error.should(include("0"))
+                }
+                "Malformed result" in {
+                  val error = intercept[InvalidResponse] {
+                    run(api.method2(""))
+                  }.getMessage.toLowerCase
+                  error.should(include("malformed result"))
+                }
               }
             }
             "Dynamic" - {
-//              "Simple API" - {
-//                "Call" in {
-//                  check { (a0: String) =>
-//                    val expected = run(simpleApi.method(a0))
-//                    execute("Dynamic / Simple API / Call", fixture.call("method", "argument" -> a0)) == expected
-//                  }
-//                }
-//                "Tell" in {
-//                  check { (a0: String) =>
-//                    execute("Dynamic / Simple API / Tell", fixture.tell("method", "argument" -> a0))
-//                    true
-//                  }
-//                }
-//                "Alias" in {
-//                  check { (a0: String) =>
-//                    val expected = run(simpleApi.method(a0))
-//                    execute("Dynamic / Simple API / Alias", fixture.call("function", "argument" -> a0)) == expected
-//                  }
-//                }
-//              }
+              "Simple API" - {
+                "Call" in {
+                  check { (a0: String) =>
+                    val expected = run(simpleApi.method(a0))
+                    execute("Dynamic / Simple API / Call", fixture.call("method", "argument" -> a0)) == expected
+                  }
+                }
+                "Tell" in {
+                  check { (a0: String) =>
+                    execute("Dynamic / Simple API / Tell", fixture.tell("method", "argument" -> a0))
+                    true
+                  }
+                }
+                "Alias" in {
+                  check { (a0: String) =>
+                    val expected = run(simpleApi.method(a0))
+                    execute("Dynamic / Simple API / Alias", fixture.call("function", "argument" -> a0)) == expected
+                  }
+                }
+              }
             }
           }
         }
