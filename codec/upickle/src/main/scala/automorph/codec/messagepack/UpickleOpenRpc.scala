2ad5417dac794b5862366897f7c2b66129f56ae3
Martin Ockajak <m@polytree.org>
2024-02-11 18:11:53 +0100

    .

--- codec/upickle/src/main/scala/automorph/codec/messagepack/UpickleOpenRpc.scala
+++ codec/upickle/src/main/scala/automorph/codec/messagepack/UpickleOpenRpc.scala
@@ -9,6 +9,7 @@ import upickle.core.{Abort, LinkedHashMap}
 private[automorph] object UpickleOpenRpc {
 
   def readWriter[Config <: UpickleMessagePackConfig](config: Config): config.ReadWriter[OpenRpc] = {
+    import config.*
 
     implicit val schemaRw: config.ReadWriter[Schema] = readwriter[Msg].bimap[Schema](fromSchema, toSchema)
     implicit val contactRw: config.ReadWriter[Contact] = config.macroRW
