293bf3890c545dfa9535484e0e32f98f3d8b5427
Martin Ockajak <m@polytree.org>
2024-02-18 16:02:04 +0100

    .

--- transport/zio-http/src/test/scala/test/transport/http/ZioHttpEndpointHttpZioTest.scala
+++ transport/zio-http/src/test/scala/test/transport/http/ZioHttpEndpointHttpZioTest.scala
@@ -1,71 +1,71 @@
-package test.transport.http
-
-import automorph.spi.{EffectSystem, EndpointTransport, RequestHandler, ServerTransport}
-import automorph.system.ZioSystem
-import automorph.transport.http.endpoint.ZioHttpEndpoint
-import org.scalacheck.Arbitrary
-import test.transport.HttpServerTest
-import test.transport.http.ZioEndpointHttpZioTest.ZioServer
-import zio.http.{Method, Routes, Server}
-import zio.{ExitCode, Promise, Runtime, Task, Unsafe, ZIO, ZIOAppDefault}
-
-class ZioHttpEndpointHttpZioTest extends HttpServerTest {
-
-  type Effect[T] = Task[T]
-  type Context = ZioHttpEndpoint.Context
-
-  override lazy val system: ZioSystem[Throwable] = {
-    implicit val runtime: Runtime[Any] = Runtime.default
-    ZioSystem()
-  }
-
-  override def run[T](effect: Effect[T]): T =
-    Unsafe.unsafe { implicit unsafe =>
-      system.runtime.unsafe.run(effect).toTry.get
-    }
-
-  override def arbitraryContext: Arbitrary[Context] =
-    HttpContextGenerator.arbitrary
-
-  override def serverTransport(fixtureId: String): ServerTransport[Effect, Context] =
-    ZioServer(system, port(fixtureId))
-
-  override def endpointTransport: EndpointTransport[Task, Context, ?] =
-    ZioHttpEndpoint(system)
-}
-
-object ZioEndpointHttpZioTest {
-
-  type Effect[T] = Task[T]
-  type Context = ZioHttpEndpoint.Context
-
-  final case class ZioServer(
-    effectSystem: EffectSystem[Effect],
-    port: Int,
-  ) extends ServerTransport[Effect, Context] with ZIOAppDefault {
-
-    private lazy val httpApp = Routes(Method.POST / "/" -> endpoint.adapter).toHttpApp
-    var server = Option.empty[Promise[Throwable, Unit]]
-    private var endpoint = ZioHttpEndpoint(effectSystem)
-
-    override def run: ZIO[Any, Throwable, Unit] =
-      Promise.make[Throwable, Unit].flatMap { promise =>
-        server = Some(promise)
-        Server.install(httpApp).flatMap { _ =>
-          println("START")
-          promise.succeed(())
-        }
-      }.flatMap(_ => ZIO.never).forkDaemon.provide(Server.defaultWithPort(port)).ignore
-
-    override def withHandler(handler: RequestHandler[Effect, Context]): ServerTransport[Effect, Context] = {
-      endpoint = endpoint.withHandler(handler)
-      this
-    }
-
-    override def init(): Effect[Unit] =
-      run.flatMap(_ => server.map(_.await.map(_ => println("INIT"))).getOrElse(ZIO.succeed(())))
-
-    override def close(): Effect[Unit] =
-      server.map(_.await.flatMap(_ => exit(ExitCode(0)))).getOrElse(ZIO.succeed(()))
-  }
-}
+//package test.transport.http
+//
+//import automorph.spi.{EffectSystem, EndpointTransport, RequestHandler, ServerTransport}
+//import automorph.system.ZioSystem
+//import automorph.transport.http.endpoint.ZioHttpEndpoint
+//import org.scalacheck.Arbitrary
+//import test.transport.HttpServerTest
+//import test.transport.http.ZioEndpointHttpZioTest.ZioServer
+//import zio.http.{Method, Routes, Server}
+//import zio.{ExitCode, Promise, Runtime, Task, Unsafe, ZIO, ZIOAppDefault}
+//
+//class ZioHttpEndpointHttpZioTest extends HttpServerTest {
+//
+//  type Effect[T] = Task[T]
+//  type Context = ZioHttpEndpoint.Context
+//
+//  override lazy val system: ZioSystem[Throwable] = {
+//    implicit val runtime: Runtime[Any] = Runtime.default
+//    ZioSystem()
+//  }
+//
+//  override def run[T](effect: Effect[T]): T =
+//    Unsafe.unsafe { implicit unsafe =>
+//      system.runtime.unsafe.run(effect).toTry.get
+//    }
+//
+//  override def arbitraryContext: Arbitrary[Context] =
+//    HttpContextGenerator.arbitrary
+//
+//  override def serverTransport(fixtureId: String): ServerTransport[Effect, Context] =
+//    ZioServer(system, port(fixtureId))
+//
+//  override def endpointTransport: EndpointTransport[Task, Context, ?] =
+//    ZioHttpEndpoint(system)
+//}
+//
+//object ZioEndpointHttpZioTest {
+//
+//  type Effect[T] = Task[T]
+//  type Context = ZioHttpEndpoint.Context
+//
+//  final case class ZioServer(
+//    effectSystem: EffectSystem[Effect],
+//    port: Int,
+//  ) extends ServerTransport[Effect, Context] with ZIOAppDefault {
+//
+//    private lazy val httpApp = Routes(Method.POST / "/" -> endpoint.adapter).toHttpApp
+//    var server = Option.empty[Promise[Throwable, Unit]]
+//    private var endpoint = ZioHttpEndpoint(effectSystem)
+//
+//    override def run: ZIO[Any, Throwable, Unit] =
+//      Promise.make[Throwable, Unit].flatMap { promise =>
+//        server = Some(promise)
+//        Server.install(httpApp).flatMap { _ =>
+//          println("START")
+//          promise.succeed(())
+//        }
+//      }.flatMap(_ => ZIO.never).forkDaemon.provide(Server.defaultWithPort(port)).ignore
+//
+//    override def withHandler(handler: RequestHandler[Effect, Context]): ServerTransport[Effect, Context] = {
+//      endpoint = endpoint.withHandler(handler)
+//      this
+//    }
+//
+//    override def init(): Effect[Unit] =
+//      run.flatMap(_ => server.map(_.await.map(_ => println("INIT"))).getOrElse(ZIO.succeed(())))
+//
+//    override def close(): Effect[Unit] =
+//      server.map(_.await.flatMap(_ => exit(ExitCode(0)))).getOrElse(ZIO.succeed(()))
+//  }
+//}
