6de6dd1e5c654c03d9f0a76a794610c6734eb905
Martin Ockajak <m@polytree.org>
2024-02-19 01:07:14 +0100

    .

--- meta/src/main/scala-3/automorph/client/RemoteInvoke.scala
+++ meta/src/main/scala-3/automorph/client/RemoteInvoke.scala
@@ -263,7 +263,7 @@ private[automorph] trait RemoteInvoke[Node, Codec <: MessageCodec[Node], Effect[
       requestContext,
     )
 
-object RemoteInvoke:
+private[automorph] object RemoteInvoke:
 
   inline def decodeResult[Node, Codec <: MessageCodec[Node], Context, R](codec: Codec): (Node, Context) => R =
     ${ decodeResultMacro[Node, Codec, Context, R]('codec) }
