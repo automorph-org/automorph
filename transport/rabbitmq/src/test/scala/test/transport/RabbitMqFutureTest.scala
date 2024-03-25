597b75a9a6edd41d5d0837eacf8fabf70cea78df
Martin Ockajak <m@polytree.org>
2024-03-25 18:00:27 +0100

    .

--- transport/rabbitmq/src/test/scala/test/transport/RabbitMqFutureTest.scala
+++ transport/rabbitmq/src/test/scala/test/transport/RabbitMqFutureTest.scala
@@ -37,7 +37,6 @@ final class RabbitMqFutureTest extends ClientServerTest with Await with Mutex {
       server.map(LocalClient[Effect, Context](system, arbitraryContext.arbitrary.sample.get, _)).getOrElse {
         throw new IllegalStateException("RPC server not defined")
       }
-      LocalClient(system, arbitraryContext.arbitrary.sample.get, server).asInstanceOf[ClientTransport[Effect, Context]]
     }
 
   override def serverTransport(fixtureId: String): ServerTransport[Effect, Context, Unit] =
