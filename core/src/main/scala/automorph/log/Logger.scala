01f45c9bc57dc0e2027f31ba2d373d4c1bcd5a3c
Martin Ockajak <m@polytree.org>
2024-12-15 16:53:34 +0100

    Refactoring

--- core/src/main/scala/automorph/log/Logger.scala
+++ core/src/main/scala/automorph/log/Logger.scala
@@ -17,10 +17,12 @@ import scala.annotation.nowarn
  *   underlying [[https://www.javadoc.io/doc/org.slf4j/slf4j-api/1.7.30/org/slf4j/Logger.html SLF4J logger]]
  */
 @SerialVersionUID(782158461L)
-private[automorph] final case class Logger private (private val underlying: slf4j.Logger) {
+final private[automorph] case class Logger private (private val underlying: slf4j.Logger) {
 
-  type Not[T] = T => Nothing
-  type Or[T, U] = Not[Not[T] & Not[U]]
+  private type Not[T] = T => Nothing
+  private type Or[T, U] = Not[Not[T] & Not[U]]
+  private type Is[T] = Not[Not[T]]
+  private type ProductOrIterable = Or[Iterable[(String, Any)], Product]
 
   def error(message: => String): Unit =
     underlying.error(message)
@@ -31,16 +33,14 @@ private[automorph] final case class Logger private (private val underlying: slf4
   def error(message: => String, properties: (String, Any)*): Unit =
     error(message, properties)
 
-  def error[T](message: => String, properties: => T)(implicit
-    evidence: Not[Not[T]] <:< Or[Iterable[(String, Any)], Product]
-  ): Unit =
+  def error[T](message: => String, properties: => T)(implicit evidence: Is[T] <:< ProductOrIterable): Unit =
     log(message, properties, underlying.isErrorEnabled, message => underlying.error(message))
 
   def error(message: => String, cause: => Throwable, properties: (String, Any)*): Unit =
     error(message, cause, properties)
 
   def error[T](message: => String, cause: => Throwable, properties: => T)(implicit
-    evidence: Not[Not[T]] <:< Or[Iterable[(String, Any)], Product]
+    evidence: Is[T] <:< ProductOrIterable
   ): Unit =
     log(message, cause, properties, underlying.isErrorEnabled, (message, cause) => underlying.error(message, cause))
 
@@ -56,16 +56,14 @@ private[automorph] final case class Logger private (private val underlying: slf4
   def warn(message: => String, properties: (String, Any)*): Unit =
     warn(message, properties)
 
-  def warn[T](message: => String, properties: => T)(implicit
-    evidence: Not[Not[T]] <:< Or[Iterable[(String, Any)], Product]
-  ): Unit =
+  def warn[T](message: => String, properties: => T)(implicit evidence: Is[T] <:< ProductOrIterable): Unit =
     log(message, properties, underlying.isWarnEnabled, message => underlying.warn(message))
 
   def warn(message: => String, cause: => Throwable, properties: (String, Any)*): Unit =
     warn(message, cause, properties)
 
   def warn[T](message: => String, cause: => Throwable, properties: => T)(implicit
-    evidence: Not[Not[T]] <:< Or[Iterable[(String, Any)], Product]
+    evidence: Is[T] <:< ProductOrIterable
   ): Unit =
     log(message, cause, properties, underlying.isWarnEnabled, (message, cause) => underlying.warn(message, cause))
 
@@ -81,16 +79,14 @@ private[automorph] final case class Logger private (private val underlying: slf4
   def info(message: => String, properties: (String, Any)*): Unit =
     info(message, properties)
 
-  def info[T](message: => String, properties: => T)(implicit
-    evidence: Not[Not[T]] <:< Or[Iterable[(String, Any)], Product]
-  ): Unit =
+  def info[T](message: => String, properties: => T)(implicit evidence: Is[T] <:< ProductOrIterable): Unit =
     log(message, properties, underlying.isInfoEnabled, message => underlying.info(message))
 
   def info(message: => String, cause: => Throwable, properties: (String, Any)*): Unit =
     info(message, cause, properties)
 
   def info[T](message: => String, cause: => Throwable, properties: => T)(implicit
-    evidence: Not[Not[T]] <:< Or[Iterable[(String, Any)], Product]
+    evidence: Is[T] <:< ProductOrIterable
   ): Unit =
     log(message, cause, properties, underlying.isInfoEnabled, (message, cause) => underlying.info(message, cause))
 
@@ -106,16 +102,14 @@ private[automorph] final case class Logger private (private val underlying: slf4
   def debug(message: => String, properties: (String, Any)*): Unit =
     debug(message, properties)
 
-  def debug[T](message: => String, properties: => T)(implicit
-    evidence: Not[Not[T]] <:< Or[Iterable[(String, Any)], Product]
-  ): Unit =
+  def debug[T](message: => String, properties: => T)(implicit evidence: Is[T] <:< ProductOrIterable): Unit =
     log(message, properties, underlying.isDebugEnabled, message => underlying.debug(message))
 
   def debug(message: => String, cause: => Throwable, properties: (String, Any)*): Unit =
     debug(message, cause, properties)
 
   def debug[T](message: => String, cause: => Throwable, properties: => T)(implicit
-    evidence: Not[Not[T]] <:< Or[Iterable[(String, Any)], Product]
+    evidence: Is[T] <:< ProductOrIterable
   ): Unit =
     log(message, cause, properties, underlying.isDebugEnabled, (message, cause) => underlying.debug(message, cause))
 
@@ -131,16 +125,14 @@ private[automorph] final case class Logger private (private val underlying: slf4
   def trace(message: => String, properties: (String, Any)*): Unit =
     trace(message, properties)
 
-  def trace[T](message: => String, properties: => T)(implicit
-    evidence: Not[Not[T]] <:< Or[Iterable[(String, Any)], Product]
-  ): Unit =
+  def trace[T](message: => String, properties: => T)(implicit evidence: Is[T] <:< ProductOrIterable): Unit =
     log(message, properties, underlying.isTraceEnabled, message => underlying.trace(message))
 
   def trace(message: => String, cause: => Throwable, properties: (String, Any)*): Unit =
     trace(message, cause, properties)
 
   def trace[T](message: => String, cause: => Throwable, properties: => T)(implicit
-    evidence: Not[Not[T]] <:< Or[Iterable[(String, Any)], Product]
+    evidence: Is[T] <:< ProductOrIterable
   ): Unit =
     log(message, cause, properties, underlying.isTraceEnabled, (message, cause) => underlying.trace(message, cause))
 
@@ -149,7 +141,7 @@ private[automorph] final case class Logger private (private val underlying: slf4
 
   @nowarn("msg=unused implicit")
   private def log[T](message: => String, properties: => T, enabled: Boolean, logMessage: String => Unit)(implicit
-    evidence: Not[Not[T]] <:< Or[Iterable[(String, Any)], Product]
+    evidence: Is[T] <:< ProductOrIterable
   ): Unit =
     if (enabled) {
       val iterableProperties = unpackProperties(properties)
@@ -165,7 +157,7 @@ private[automorph] final case class Logger private (private val underlying: slf4
     properties: => T,
     enabled: Boolean,
     logMessage: (String, Throwable) => Unit,
-  )(implicit evidence: Not[Not[T]] <:< Or[Iterable[(String, Any)], Product]): Unit =
+  )(implicit evidence: Is[T] <:< ProductOrIterable): Unit =
     if (enabled) {
       val iterableProperties = unpackProperties(properties)
       addDiagnosticContext(iterableProperties)
@@ -175,8 +167,8 @@ private[automorph] final case class Logger private (private val underlying: slf4
 
   private def unpackProperties[T](properties: => T): Iterable[(String, Any)] =
     properties match {
-      case product: Product => productProperties(product)
-      case iterable: Iterable[?] => iterable.asInstanceOf[Iterable[(String, Any)]]
+      case product if product.isInstanceOf[Product] => productProperties(product.asInstanceOf[Product])
+      case iterable if iterable.isInstanceOf[Iterable[?]] => iterable.asInstanceOf[Iterable[(String, Any)]]
       case _ => Iterable()
     }
 
